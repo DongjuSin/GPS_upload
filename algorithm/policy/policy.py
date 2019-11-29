d465f0fc2ce1a76e5711b49ae6c6ff5738481849
alish212 <44188860+alish212@users.noreply.github.com>
2019-11-29 17:12:39 +0900

    Add files via upload

---
+++ algorithm/policy/policy.py
@@ -0,0 +1,29 @@
+""" This file defines the base class for the policy. """
+import abc
+
+
+class Policy(object):
+    """ Computes actions from states/observations. """
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    #def act(self, x, obs, t, noise):
+    def act(self, x, obs, t, noise, condition):
+        """
+        Args:
+            x: State vector.
+            obs: Observation vector.
+            t: Time step.
+            noise: A dU-dimensional noise vector.
+        Returns:
+            A dU dimensional action vector.
+        """
+        raise NotImplementedError("Must be implemented in subclass.")
+
+    def set_meta_data(self, meta):
+        """
+        Set meta data for policy (e.g., domain image, multi modal observation sizes)
+        Args:
+            meta: meta data.
+        """
+        return
